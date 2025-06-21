12058cbf40e25e2a5f8c3bad3dcaf5376b48155d
Sal_ <salvatore.pontillo@stud.edubs.ch>
2025-06-21 17:57:42 +0200

    ver. 0.2

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
     const openButton = document.getElementById("openButton");
     const div3 = document.querySelector(".div3");
+    const todolist = document.getElementById("todoList");
 
     openButton.addEventListener("click", () => {
-        
+
         // Ersetzt jetzigen Content
         div3.innerHTML = `
-            <h3>New Content</h3>
-            <p>This content has been dynamically updated!</p>
+            <h3>Neues Todo einfügen</h3>
+            <input type="text" id="todoInput" placeholder="titel"/>
+            <button id="addTodoButton">Hinzufuegen</button>
             <button id="schliessenButton">Schliessen</button>
         `;
 
-        // Add an event listener to the reset button
+        // Todos Einfügen funktion
+        const addTodoButton = div3.querySelector("#addTodoButton");
+        const todoInput = div3.querySelector("#todoInput");
+
+        addTodoButton.addEventListener("click", () => {
+                const neuTodo = todoInput.value;
+
+                if (neuTodo) {
+                    //Todo herstellen
+                    const listItem = document.createElement("li");
+                    listItem.textContent = neuTodo;
+                    console.log(listItem);
+
+                    //Todo in der Liste einfügen
+                    todolist.appendChild(listItem);
+
+                    //Input ausleeren
+                    todoInput.value = "";
+                } else {
+                    alert("Bitte nicht leer eingeben")
+                }
+            }
+        );
+
+        // Schliessen Button funktion
         const schliessenButton = div3.querySelector("#schliessenButton");
         schliessenButton.addEventListener("click", () => {
             div3.innerHTML = `
